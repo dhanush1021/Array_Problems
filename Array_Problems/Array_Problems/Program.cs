aa67648a5a520a59e340757671ac134af65d4560
Dhanush <dhanushdybala10@gmail.com>
2024-01-22 19:37:21 +0530

    [Dhansuh] Add: to Reverse Each Word in a Given String in C#

--- Array_Problems/Array_Problems/Program.cs
+++ Array_Problems/Array_Problems/Program.cs
@@ -10,26 +10,12 @@ namespace Array_Problems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a Number to Print the pattern");
-            int n = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    if(i==0 || i == n - 1)
-                    {
-                        Console.Write("*");
-                    }
-                    else if(j==0 || j == n - 1)
-                    {
-                        Console.Write("*");
-                    }
-                    else { 
-                        Console.Write(" ");
-                    }
-                }
-                Console.WriteLine();
-            }
+            Console.WriteLine("Enter a string");
+            string str = Console.ReadLine();
+            string[] list = str.Split(' ');
+            Array.Reverse(list);
+            string strrev = string.Join(" ", list);
+            Console.WriteLine(strrev);
         }
     }
 }
