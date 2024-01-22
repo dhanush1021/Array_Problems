6ac18efd00d24322361a0dad55caf6084c56beb7
Dhanush <dhanushdybala10@gmail.com>
2024-01-22 18:55:17 +0530

    [Dhansuh] Add: To count a total number of duplicate elements in an array.

---
+++ Array_Problems/Array_Problems/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() {1,2,3,1,2,3,7,5,3,7,5,0,6,3,4 };
+            SortedDictionary<int,int> sort = new SortedDictionary<int,int>();
+            foreach (int i in list)
+            {
+                if (sort.ContainsKey(i)) 
+                    sort[i]++;
+                else 
+                    sort[i] = 1;
+            }
+            list=list.Distinct().ToList();
+            list.Sort();
+            foreach (int i in list)
+            {
+                Console.WriteLine($"{i} : {sort[i]}");
+            }
+        }
+    }
+}
